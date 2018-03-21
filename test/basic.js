640954a8c9e9472d3fa9b5cf4534e7f1fecf48f7
Stephen Whitmore <sww@eight45.net>
2018-03-20 17:32:44 -0700

    test: base error case

--- test/basic.js
+++ test/basic.js
@@ -11,38 +11,61 @@ function emptyFixture (key, done) {
     key = null
   }
   var db = null
-  tmp(function (err, dir) {
+  tmp(function (err, dir, cleanup) {
+    if (err) return done(err)
     db = key ? hyperdb(dir, key) : hyperdb(dir)
-    done(err, db)
+    db.ready(function () {
+      done(null, db, dir, cleanup)
+    })
   })
 }
 
 function fixture (done) {
-  var db = null
-  tmp(function (err, dir) {
-    db = hyperdb(dir)
+  emptyFixture(function (err, db, dir, cleanup) {
     db.put('/hello', 'world', function(err, node) {
-      if (err) return fin(err)
-      db.put('/hello', 'warld', function(err, node) {
-        done(err, db)
+      if (err) return done(err)
+      db.put('/hello', 'there', function(err, node) {
+        if (err) return done(err)
+        done(err, db, dir, cleanup)
       })
     })
   })
 }
 
-test('fresh tarball', function (t) {
+test('different shared keys', function (t) {
+  emptyFixture(function (err, db0, dir0, cleanup0) {
+    t.notOk(err)
+    emptyFixture(function (err, db1, dir1, cleanup1) {
+      t.notOk(err)
+      replicate(db0, dir1, function (err) {
+        t.ok(err)
+        t.equals(err.message, 'shared hyperdb keys do not match')
+        t.end()
+      })
+    })
+  })
+})
+
+/*
+
+test('existing local; fresh remote', function (t) {
   t.plan(6)
 
-  fixture(function (err, db) {
+  fixture(function (err, db0, cleanup0) {
     t.notOk(err)
 
-    tmp(function (err, dir) {
-      var tgz = path.join(dir, 'db.tar.gz')
-      t.notOk(fs.existsSync(tgz))
+    tmp(function (err, dir, cleanup1) {
+      t.notOk(fs.existsSync(dir))
 
-      replicate(db, tgz, function (err) {
+      replicate(db0, dir, function (err) {
         t.notOk(err)
-        t.ok(fs.existsSync(tgz))
+        t.ok(fs.existsSync(dir))
+
+        var db1 = hyperdb(dir, db0.key)
+
+        // move below into a reusable function
+        // also have it check that the new hyperdb is authorized by doing a
+        // write + replicate + 'db.get()'
 
         var idx = 0
         db.createHistoryStream()
@@ -55,12 +78,15 @@ test('fresh tarball', function (t) {
               idx++
             }
           })
+
+        cleanup0()
+        cleanup1()
       })
     })
   })
 })
 
-test('existing tarball', function (t) {
+test('fresh local; existing remote', function (t) {
   t.plan(7)
 
   // create a fixture /w test data and write it
@@ -101,32 +127,7 @@ test('existing tarball', function (t) {
   }
 })
 
-test('safe write', function (t) {
-  t.plan(6)
+// TODO: existing local; existing remote
+// ...
 
-  fixture(function (err, db) {
-    t.notOk(err)
-
-    tmp(function (err, dir) {
-      var tgz = path.join(dir, 'db.tar.gz')
-      t.notOk(fs.existsSync(tgz))
-
-      replicate(db, { safetyFile: true }, tgz, function (err) {
-        t.notOk(err)
-        t.ok(fs.existsSync(tgz))
-
-        var idx = 0
-        db.createHistoryStream()
-          .on('data', function (node) {
-            if (idx === 0) {
-              t.equal(node.value.toString(), 'world')
-              idx++
-            } else if (idx === 1) {
-              t.equal(node.value.toString(), 'warld')
-              idx++
-            }
-          })
-      })
-    })
-  })
-})
+*/
